47783f9d2cf62b3ec94012eba70a5a311667f84e
willfeng27 <willfeng@uw.edu>
2024-10-13 01:14:44 -0700

    Login Page

--- .venv/app.py
+++ .venv/app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask('Hangouter')
 
+
 @app.route("/")
+# uses index.html webpage
 def home():
     return render_template('index.html')
+
+
+# returns username and password
+def form():
+    return render_template_string(open('login.html').read())
+
+
 @app.route("/schedule")
 def schedule():
     return render_template('')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+# handles the submission of the login page
+@app.route('/submit', methods=["GET"])
+def submit():
+    username = request.args.get["username"]
+    password = request.args.get["password"]
+    print(username)
+    print(password)
