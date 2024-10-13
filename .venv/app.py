e5865fc412d54216f92a21cbcc0d08167459c09c
senlinsun <carblue1@gmail.com>
2024-10-12 20:41:25 -0700

    created calendar page

--- .venv/app.py
+++ .venv/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 app = Flask('Hangouter')
 
+events = [
+    {
+        'activityName' : 'sample Name',
+        'date' : '2024-10-13',
+    },
+    {
+        'activityName' : 'sleepy',
+        'date' : '2024-10-12',
+    }
+]
+
 @app.route("/")
 def home():
     return render_template('index.html')
 @app.route("/schedule")
 def schedule():
-    return render_template('')
+    return render_template('schedule.html', events = events)
